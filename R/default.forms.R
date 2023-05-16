86f839f92a3376ef8739b2af8c5f8ebceaa51c3a
femeunier <felicien.meunier@gmail.com>
2023-05-16 19:13:10 +0200

    Add missing files

--- R/default.forms.R
+++ R/default.forms.R
@@ -1,78 +1,202 @@
-default.forms <- function(names,
-                          fixed.effect = TRUE,
-                          random.effect = FALSE){
+default.forms <- function(names = c("weibull","power","gmm"),
+                          fixed.effect = "all",
+                          random.effect = "all"){
 
   form.list <- list()
+  f.functional.form <- list(
+    power = "h ~ a*(dbh**b)",
+    weibull = "h ~ a*(1-exp(-b*(dbh**k)))",
+    gmm = "h ~ (a*(dbh**b))/(k + dbh**b)")
 
+
+  if (any(fixed.effect == "all")){
+    fixed.effect.params <- c("a","b","k")
+  } else {
+    fixed.effect.params <- fixed.effect
+  }
+
+  if (any(fixed.effect == "all")){
+    fixed.effect.params <- c("a","b","k")
+  } else {
+    fixed.effect.params <- fixed.effect
+  }
+
+  if (any(random.effect == "all")){
+    random.effect.params <- c("a","b","k")
+  } else {
+    random.effect.params <- random.effect
+  }
+
+
+  mixed.effect.params <- intersect(fixed.effect.params,random.effect.params)
+  fixed.effect.params <- setdiff(fixed.effect.params, mixed.effect.params)
+  random.effect.params <- setdiff(random.effect.params, mixed.effect.params)
 
   # Power function form
   if ("power" %in% tolower(names)){
 
-    if (fixed.effect & !random.effect){
+    cmixed.effect.params <- mixed.effect.params[mixed.effect.params %in% c("a","b")]
+    cfixed.effect.params <- fixed.effect.params[fixed.effect.params %in% c("a","b")]
+    crandom.effect.params <- random.effect.params[random.effect.params %in% c("a","b")]
 
-      form.list[["power"]] <- brmsformula(h ~ a*(dbh**b),
-                                          a + b  ~ 1 + liana.cat,
-                                          nl = TRUE)
-    } else if (!fixed.effect & random.effect){
-
-      form.list[["power"]] <- brmsformula(h ~ a*(dbh**b),
-                                          a + b  ~ 1 + (1 | sp),
-                                          nl = TRUE)
-
-    } else if (fixed.effect & random.effect){
-      form.list[["power"]] <- brmsformula(h ~ a*(dbh**b),
-                                          a + b ~ 1 + liana.cat + (1 | sp),
-                                          nl = TRUE)
+    if (length(cmixed.effect.params) == 0){
+      f.mixed.effect <- cmixed.effect.params
+    } else{
+      f.mixed.effect <- paste(paste(cmixed.effect.params, collapse = " + "),
+                              "~ 1 + liana.cat + (1 | sp)")
     }
+
+    if (length(cfixed.effect.params) == 0){
+      f.fixed.effect <- cfixed.effect.params
+    } else{
+      f.fixed.effect <- paste(paste(cfixed.effect.params, collapse = " + "),
+                              "~ 1 + liana.cat")
+    }
+
+    if (length(crandom.effect.params) == 0){
+      f.random.effect <- crandom.effect.params
+    } else{
+      f.random.effect <- paste(paste(crandom.effect.params, collapse = " + "),
+                              "~ 1 + (1 | sp)")
+    }
+
+
+    all.effects <- c(f.mixed.effect,
+                     f.fixed.effect,
+                     f.random.effect)
+
+    if (length(all.effects) == 1){
+
+      form.list[["power"]] <- brmsformula(f.functional.form[["power"]],
+                                            all.effects[1],
+                                            nl = TRUE)
+    } else if (length(all.effects == 2)){
+
+      form.list[["power"]] <- brmsformula(f.functional.form[["power"]],
+                                            all.effects[1],all.effects[2],
+                                            nl = TRUE)
+    } else if (length(all.effects == 3)){
+
+      form.list[["power"]] <- brmsformula(f.functional.form[["power"]],
+                                            all.effects[1],all.effects[2],all.effects[3],
+                                            nl = TRUE)
+    } else {
+      form.list[["power"]] <- brmsformula(f.functional.form[["power"]],
+                                            nl = TRUE)
+    }
+
+
   }
 
+  # Weibull
   if ("weibull" %in% tolower(names)){
+    cmixed.effect.params <- mixed.effect.params[mixed.effect.params %in% c("a","b","k")]
+    cfixed.effect.params <- fixed.effect.params[fixed.effect.params %in% c("a","b","k")]
+    crandom.effect.params <- random.effect.params[random.effect.params %in% c("a","b","k")]
 
-    # Weibull
-    if (fixed.effect & !random.effect){
+    if (length(cmixed.effect.params) == 0){
+      f.mixed.effect <- cmixed.effect.params
+    } else{
+      f.mixed.effect <- paste(paste(cmixed.effect.params, collapse = " + "),
+                              "~ 1 + liana.cat + (1 | sp)")
+    }
 
-      form.list[["weibull"]] <- brmsformula(h ~ a*(1-exp(-b*(dbh**k))),
-                                            a + b + k ~ 1 + liana.cat,
+    if (length(cfixed.effect.params) == 0){
+      f.fixed.effect <- cfixed.effect.params
+    } else{
+      f.fixed.effect <- paste(paste(cfixed.effect.params, collapse = " + "),
+                              "~ 1 + liana.cat")
+    }
+
+    if (length(crandom.effect.params) == 0){
+      f.random.effect <- crandom.effect.params
+    } else{
+      f.random.effect <- paste(paste(crandom.effect.params, collapse = " + "),
+                               "~ 1 + (1 | sp)")
+    }
+
+    all.effects <- c(f.mixed.effect,
+                     f.fixed.effect,
+                     f.random.effect)
+
+    if (length(all.effects) == 1){
+
+      form.list[["weibull"]] <- brmsformula(f.functional.form[["weibull"]],
+                                            all.effects[1],
                                             nl = TRUE)
-    } else if (!fixed.effect & random.effect){
+    } else if (length(all.effects == 2)){
 
-
-      form.list[["weibull"]] <- brmsformula(h ~ a*(1-exp(-b*(dbh**k))),
-                                            a + b + k ~ 1 + (1 | sp),
+      form.list[["weibull"]] <- brmsformula(f.functional.form[["weibull"]],
+                                            all.effects[1],all.effects[2],
                                             nl = TRUE)
+    } else if (length(all.effects == 3)){
 
-    } else if (fixed.effect & random.effect){
-
-
-      form.list[["weibull"]] <- brmsformula(h ~ a*(1-exp(-b*(dbh**k))),
-                                            a + b + k ~ 1 + liana.cat + (1 | sp),
+      form.list[["weibull"]] <- brmsformula(f.functional.form[["weibull"]],
+                                            all.effects[1],all.effects[2],all.effects[3],
+                                            nl = TRUE)
+    } else {
+      form.list[["weibull"]] <- brmsformula(f.functional.form[["weibull"]],
                                             nl = TRUE)
     }
+
+
   }
 
+  # gMM
   if ("gmm" %in% tolower(names)){
 
-    # gMM
+    cmixed.effect.params <- mixed.effect.params[mixed.effect.params %in% c("a","b","k")]
+    cfixed.effect.params <- fixed.effect.params[fixed.effect.params %in% c("a","b","k")]
+    crandom.effect.params <- random.effect.params[random.effect.params %in% c("a","b","k")]
 
-    if (fixed.effect & !random.effect){
-
-      form.list[["gmm"]] <- brmsformula(h ~ (a*(dbh**b))/(k + dbh**b),
-                                        a + b + k ~ 1 + liana.cat,
-                                        nl = TRUE)
-    } else if (!fixed.effect & random.effect){
-
-
-      form.list[["gmm"]] <- brmsformula(h ~ (a*(dbh**b))/(k + dbh**b),
-                                        a + b + k ~ 1 + (1 | sp),
-                                        nl = TRUE)
-
-    } else if (fixed.effect & random.effect){
-
-
-      form.list[["gmm"]] <- brmsformula(h ~ (a*(dbh**b))/(k + dbh**b),
-                                        a + b + k ~ 1 + liana.cat + (1 | sp),
-                                        nl = TRUE)
+    if (length(cmixed.effect.params) == 0){
+      f.mixed.effect <- cmixed.effect.params
+    } else{
+      f.mixed.effect <- paste(paste(cmixed.effect.params, collapse = " + "),
+                              "~ 1 + liana.cat + (1 | sp)")
     }
+
+
+    if (length(cfixed.effect.params) == 0){
+      f.fixed.effect <- cfixed.effect.params
+    } else{
+      f.fixed.effect <- paste(paste(cfixed.effect.params, collapse = " + "),
+                              "~ 1 + liana.cat")
+    }
+
+    if (length(crandom.effect.params) == 0){
+      f.random.effect <- crandom.effect.params
+    } else{
+      f.random.effect <- paste(paste(crandom.effect.params, collapse = " + "),
+                               "~ 1  + (1 | sp)")
+    }
+
+    all.effects <- c(f.mixed.effect,
+                     f.fixed.effect,
+                     f.random.effect)
+
+    if (length(all.effects) == 1){
+
+      form.list[["gmm"]] <- brmsformula(f.functional.form[["gmm"]],
+                                            all.effects[1],
+                                            nl = TRUE)
+    } else if (length(all.effects == 2)){
+
+      form.list[["gmm"]] <- brmsformula(f.functional.form[["gmm"]],
+                                            all.effects[1],all.effects[2],
+                                            nl = TRUE)
+    } else if (length(all.effects == 3)){
+
+      form.list[["gmm"]] <- brmsformula(f.functional.form[["gmm"]],
+                                            all.effects[1],all.effects[2],all.effects[3],
+                                            nl = TRUE)
+    } else {
+      form.list[["gmm"]] <- brmsformula(f.functional.form[["gmm"]],
+                                            nl = TRUE)
+    }
+
+
+
   }
 
   return(form.list)
--- R/default.priors.R
+++ R/default.priors.R
@@ -1,32 +1,64 @@
-default.priors <- function(names){
+default.priors <- function(names,
+                           params = "all"){
 
 
-  priors.list <- form.list <- list()
+  priors.list <- list()
 
   if ("power" %in% tolower(names)){
 
+    priors.list[["power"]] <- empty_prior()
+
     # Power
-    priors.list[["power"]] <- c(set_prior("normal(62,20)",  nlpar = "a",lb = 0),
-                                set_prior("normal(0.0352,0.02)", nlpar = "b",lb = 0))
-
-
+    if ("all" %in% tolower(params)){
+      priors.list[["power"]] <- c(set_prior("normal(2.98,0.3)",  nlpar = "a"),
+                                  set_prior("normal(0.56,0.05)", nlpar = "b"))
+    } else if ("a" %in% tolower(params)){
+      priors.list[["power"]] <- priors.list[["power"]] + set_prior("normal(2.98,0.3)",  nlpar = "a")
+    } else if ("b" %in% tolower(params)){
+      priors.list[["power"]] <- priors.list[["power"]] + set_prior("normal(0.56,0.05)", nlpar = "b")
+    }
   }
 
   if ("weibull" %in% tolower(names)){
 
+    priors.list[["weibull"]] <- empty_prior()
+
     # Weibull
-    priors.list[["weibull"]] <- c(set_prior("normal(62,20)",  nlpar = "a",lb = 0),
-                                  set_prior("normal(0.0352,0.02)", nlpar = "b",lb = 0),
-                                  set_prior("normal(0.694,0.3)", nlpar = "k",lb = 0))
+    if ("all" %in% tolower(params)){
+      priors.list[["weibull"]] <- c(set_prior("normal(62,20)",  nlpar = "a"),
+                                    set_prior("normal(0.0352,0.02)", nlpar = "b"),
+                                    set_prior("normal(0.694,0.3)", nlpar = "k"))
+
+    } else if ("a" %in% tolower(params)){
+      priors.list[["weibull"]] <- priors.list[["weibull"]] + set_prior("normal(62,20)",  nlpar = "a")
+
+    } else if ("b" %in% tolower(params)){
+      priors.list[["weibull"]] <- priors.list[["weibull"]] + set_prior("normal(0.0352,0.02)", nlpar = "b")
+
+    } else if ("k" %in% tolower(params)){
+      priors.list[["weibull"]] <- priors.list[["weibull"]] + set_prior("normal(0.694,0.3)", nlpar = "k")
+    }
   }
 
   if ("gmm" %in% tolower(names)){
 
-    # gMM
-    priors.list[["gmm"]] <- c(set_prior("normal(58,10)",  nlpar = "a",lb = 0),
-                              set_prior("normal(0.73,0.2)", nlpar = "b",lb = 0),
-                              set_prior("normal(22,5)", nlpar = "k",lb = 0))
+    priors.list[["gmm"]] <- empty_prior()
 
+    # gMM
+    if ("all" %in% tolower(params)){
+      priors.list[["gmm"]] <- c(set_prior("normal(58,10)",  nlpar = "a"),
+                                set_prior("normal(0.73,0.2)", nlpar = "b"),
+                                set_prior("normal(22,5)", nlpar = "k"))
+
+    } else if ("a" %in% tolower(params)){
+      priors.list[["gmm"]] <- priors.list[["gmm"]] + set_prior("normal(58,10)",  nlpar = "a")
+
+    } else if ("b" %in% tolower(params)){
+      priors.list[["gmm"]] <- priors.list[["gmm"]] + set_prior("normal(0.73,0.2)", nlpar = "b")
+
+    } else if ("k" %in% tolower(params)){
+      priors.list[["gmm"]] <- priors.list[["gmm"]] + set_prior("normal(22,5)", nlpar = "k")
+    }
   }
 
   return(priors.list)
