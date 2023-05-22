ffa0898d2e0197535f5412600fc3f40832a59eb2
femeunier <felicien.meunier@gmail.com>
2023-05-22 18:02:24 +0200

    Remove lb

--- R/default.priors.R
+++ R/default.priors.R
@@ -6,8 +6,8 @@ default.priors <- function(names,
 
   if ("power" %in% tolower(names)){
 
-    priors.list[["power"]] <- c(set_prior("normal(2.98,0.3)",  nlpar = "a", lb = 0),
-                                set_prior("normal(0.56,0.05)", nlpar = "b", lb = 0))
+    priors.list[["power"]] <- c(set_prior("normal(2.98,0.3)",  nlpar = "a", coef = "Intercept"),
+                                set_prior("normal(0.56,0.05)", nlpar = "b", coef = "Intercept"))
 
     # Power
     if ("all" %in% tolower(params)){
@@ -16,22 +16,25 @@ default.priors <- function(names,
                                   set_prior("normal(0,0.3)",  nlpar = "a", coef = "liana.cathigh") +
                                   set_prior("normal(0,0.05)",  nlpar = "b", coef = "liana.catlow") +
                                   set_prior("normal(0,0.05)",  nlpar = "b", coef = "liana.cathigh")
+
     } else if ("a" %in% tolower(params)){
       priors.list[["power"]] <- priors.list[["power"]] +
         set_prior("normal(0,0.3)",  nlpar = "a", coef = "liana.catlow") +
         set_prior("normal(0,0.3)",  nlpar = "a", coef = "liana.cathigh")
+
     } else if ("b" %in% tolower(params)){
       priors.list[["power"]] <- priors.list[["power"]] +
         set_prior("normal(0,0.05)",  nlpar = "b", coef = "liana.catlow") +
         set_prior("normal(0,0.05)",  nlpar = "b", coef = "liana.cathigh")
+
     }
   }
 
   if ("weibull" %in% tolower(names)){
 
-    priors.list[["weibull"]] <- c(set_prior("normal(62,20)",  nlpar = "a", lb = 0),
-                                  set_prior("normal(0.0352,0.02)", nlpar = "b", lb = 0),
-                                  set_prior("normal(0.694,0.3)", nlpar = "k", lb = 0))
+    priors.list[["weibull"]] <- c(set_prior("normal(62,20)",  nlpar = "a", coef = "Intercept"),
+                                  set_prior("normal(0.0352,0.02)", nlpar = "b", coef = "Intercept"),
+                                  set_prior("normal(0.694,0.3)", nlpar = "k", coef = "Intercept"))
 
     # Weibull
     if ("all" %in% tolower(params)){
@@ -66,9 +69,9 @@ default.priors <- function(names,
 
   if ("gmm" %in% tolower(names)){
 
-    priors.list[["gmm"]] <- c(set_prior("normal(58,10)",  nlpar = "a", lb = 0),
-                              set_prior("normal(0.73,0.2)", nlpar = "b", lb = 0),
-                              set_prior("normal(22,5)", nlpar = "k", lb = 0))
+    priors.list[["gmm"]] <- c(set_prior("normal(58,10)",  nlpar = "a", coef = "Intercept"),
+                              set_prior("normal(0.73,0.2)", nlpar = "b", coef = "Intercept"),
+                              set_prior("normal(22,5)", nlpar = "k", coef = "Intercept"))
 
     # gMM
     if ("all" %in% tolower(params)){
