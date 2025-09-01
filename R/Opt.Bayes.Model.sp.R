16ce5d61319b558dfcba79d1a1cd14245f7e2465
femeunier <felicien.meunier@gmail.com>
2025-09-01 13:36:11 +0200

    Correction

--- R/Opt.Bayes.Model.sp.R
+++ R/Opt.Bayes.Model.sp.R
@@ -1,50 +1,157 @@
-write.script.sp <- function(file.name,
-                         dir.name,
-                         sp.name,
-                         settings.location,
-                         strong = FALSE,
-                         site.re = FALSE,
-                         threads = FALSE){
+Opt.Bayes.Model.sp <- function(dir.name,
+                            settings,
+                            site.name,
+                            strong = FALSE,
+                            site.re = FALSE,
+                            threads = FALSE,
+                            nested = FALSE,
+                            crossed = FALSE,
+                            include.re.prior = "none"){
 
-  file <- file.path(dir.name,file.name)
+  Names <- settings[["Names"]]
+  fixed.effect.2.test <- settings[["fixed.effect.2.test"]]
+  overwrite <- settings[["overwrite"]]
+  re <- settings[["re"]]
+  Nchains <- settings[["Nchains"]]
+  Niter <- settings[["Niter"]]
+  warmup <- settings[["warmup"]]
 
-  writeLines("rm(list = ls())",con = file)
-  write("",file=file,append=TRUE)
-  write("LianaRemovalRevisited::load.everything()",file=file,append=TRUE)
-  write("",file=file,append=TRUE)
+  make.a.copy <- settings[["make.a.copy"]]
+  if (is.null(make.a.copy)){
+    make.a.copy <- FALSE
+  }
 
-  write(paste0("settings <- readRDS(\"",
-               settings.location,
-               "\")"),file=file,append=TRUE)
+  if (is.null(warmup)){
+    warmup <- floor(Niter/2)
+  }
 
-  write(paste0("dir.name <- \"",dir.name,"\""),
-        file=file,append=TRUE)
+  init <- settings[["init"]]
+  if (is.null(init)){
+    init <- 0
+  }
 
-  write(paste0("site.name <- \"",site.name,"\""),
-        file=file,append=TRUE)
+  init_r <- settings[["init_r"]]
+
+  if (is.null(init_r)){
+    init_r <- 2
+  }
 
 
-  write("",file=file,append=TRUE)
+  refresh <- settings[["refresh"]]
 
-  if (!threads){
-    if (strong & site.re){
-      write("Opt.Bayes.Model.sp(dir.name,settings,site.name,TRUE,TRUE)",file=file,append=TRUE)
-    } else if (strong & !site.re){
-      write("Opt.Bayes.Model.sp(dir.name,settings,site.name,TRUE,FALSE)",file=file,append=TRUE)
-    } else if (!strong & site.re) {
-      write("Opt.Bayes.Model.sp(dir.name,settings,site.name,FALSE,TRUE)",file=file,append=TRUE)
-    } else {
-      write("Opt.Bayes.Model.sp(dir.name,settings,site.name)",file=file,append=TRUE)
-    }
-  } else {
-    if (strong & site.re){
-      write("Opt.Bayes.Model.sp(dir.name,settings,site.name,TRUE,TRUE,TRUE)",file=file,append=TRUE)
-    } else if (strong & !site.re){
-      write("Opt.Bayes.Model.sp(dir.name,settings,site.name,TRUE,FALSE,TRUE)",file=file,append=TRUE)
-    } else if (!strong & site.re) {
-      write("Opt.Bayes.Model.sp(dir.name,settings,site.name,FALSE,TRUE,TRUE)",file=file,append=TRUE)
-    } else {
-      write("Opt.Bayes.Model.sp(dir.name,settings,site.name,threads = TRUE)",file=file,append=TRUE)
+  if (is.null(refresh)){
+    refresh <- round(Niter/10)
+  }
+
+  thin <- settings[["thin"]]
+
+  if (is.null(thin)){
+    thin <- 1
+  }
+
+  control.list <- settings[["control.list"]]
+  backend <- settings[["backend"]]
+
+  data.file <- file.path(dir.name,
+                         paste0("data_",site.name,".RDS"))
+
+  data <- readRDS(data.file)
+
+  for (model in Names){
+
+    cfixed.effect.2.test <- fixed.effect.2.test[[model]]
+
+    for (model.form in seq(1,length(cfixed.effect.2.test))){
+
+      print(paste(site.name,"-",
+                  paste0("Model (",model,"):"),paste0(which(Names == model),"/",length(Names)),"-",
+                  paste0("Model Form (",paste(cfixed.effect.2.test[[model.form]],collapse = ""),"):"),paste0(model.form,"/",length(cfixed.effect.2.test))
+      )
+      )
+
+      cname <- paste(model,paste(cfixed.effect.2.test[[model.form]],collapse = ""),sep = "_")
+      op.file <- file.path(dir.name,
+                           paste0("Fit.",site.name,".",cname,".RDS"))
+
+      if (!overwrite & file.exists(op.file)){
+        next()
+      }
+
+      form.list <- default.forms.sp(names = Names,
+                                    fixed.effect = cfixed.effect.2.test[[model.form]],
+                                    random.effect = re,
+                                    site.re = site.re,
+                                    nested = nested,
+                                    crossed = crossed)
+
+      priors.list <- default.priors(names = model,
+                                    fixed.effect = cfixed.effect.2.test[[model.form]],
+                                    random.effect = include.re.prior,
+                                    strong = strong)
+
+
+      existing.cat <- paste0("liana.cat",unique(data %>%
+                                                  filter(liana.cat != "no") %>%
+                                                  pull(liana.cat)))
+
+      for (i in seq(1,length(priors.list))){
+        priors.list[[i]] <- priors.list[[i]]  %>%
+          filter(coef %in% c("","Intercept",existing.cat))
+      }
+
+      if (!(threads)){
+        cfit <- brm(form.list[[model]],
+                    data=data %>%
+                      mutate(logh = log(h),
+                             delta = 1e-5,
+                             sp = as.factor(sp)),
+                    cores = min(Nchains,
+                                parallel::detectCores() - 1),
+                    prior = priors.list[[model]],
+                    control = control.list,
+                    init = init,
+                    init_r = init_r,
+                    chains = Nchains,
+                    backend = backend,
+                    refresh = refresh,
+                    iter = Niter,
+                    warmup = warmup,
+                    silent = 2)
+      } else{
+        cfit <- brm(form.list[[model]],
+                    data=data %>%
+                      mutate(logh = log(h),
+                             delta = 1e-5,
+                             sp = as.factor(sp)),
+                    cores = Nchains,
+                    prior = priors.list[[model]],
+                    init = init,
+                    init_r = init_r,
+                    control = control.list,
+                    chains = Nchains,
+                    threads = threading(floor(parallel::detectCores()/Nchains)),
+                    iter = Niter,
+                    refresh = refresh,
+                    warmup = warmup,
+                    backend = backend,
+                    silent = 2)
+      }
+
+      does.file.exist <- file.exists(op.file)
+      count <- 1
+
+      if (file.exists(op.file) & make.a.copy){
+        while (does.file.exist){
+          op.file <- file.path(dir.name,
+                               paste0("Fit.",site.name,".",cname,".",count,".RDS"))
+          count = count + 1
+
+          does.file.exist <- file.exists(op.file)
+        }
+      }
+
+      saveRDS(cfit,
+              op.file)
     }
   }
 }
